176575d6600d3cd1ce711ed316ea62cb6a0e4a07
DrJanPanNees <JanPanNees@yahoo.com>
2026-02-15 10:15:18 +0100

    Test af automatisk deploy

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello from CI/CD Pipeline!");
+app.MapGet("/", () => "🚀 CI/CD VIRKER! Opdateret kl. " + DateTime.Now.ToLongTimeString());
 app.MapGet("/healthz", () => Results.Ok("Healthy")); 
 
 app.MapGet("/", () => "Hello World!");
